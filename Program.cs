8bdf99ae987db7bbe4b0672098370cc5ea1de723
DanyaIT <105100908+DanyaIT@users.noreply.github.com>
2022-11-11 17:23:36 +0300

    create two [] and write function FillArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] arr1 = new string [5] {"hello", "2", "world", "add","egg"};
+string [] arr2 = new string [arr1.Length];
+
+string [] FillArray(string [] arr1, string [] arr2){
+    int count = 0;
+    for(int i = 0; i<arr1.Length; i++){
+        if(arr1[i].Length<=3){
+            arr2[count] = arr1[i];
+            count ++;
+        }
+    }
+    return arr2;
+}
